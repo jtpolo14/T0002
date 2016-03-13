0f7d0b807e7cd6825e574eaf7a27367cf77d5b9f
jtpolo14 <jtpolo14@gmail.com>
2016-03-12 19:49:53 -0800

    Updates: -c2.py to read txt file and get stock symbols and send request to server -process02.py to call the Quandl api and update mongodb with stock records

--- c2.py
+++ c2.py
@@ -1,12 +1,45 @@
 #!/usr/bin/python
 
 import socket
+import datetime
+import time
+import pytz
+import threading
 
-s = socket.socket()
-host = socket.gethostname()
-port = 12345
 
-s.connect((host, port))
-s.sendall('02')
-print s.recv(1024)
-s.close()
+def send_request(sym):
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 12345
+
+    s.connect((host, port))
+    print sym
+    s.sendall('02{0}'.format(sym))
+    print s.recv(1024)
+    s.close()
+
+
+def next_dt(dt, mins):
+    nsecs = dt.minute*60 + dt.second + dt.microsecond *1e-6
+    delta = (nsecs//mins)*mins + mins - nsecs
+    return dt + datetime.timedelta(seconds=delta)
+
+
+def test(sym):
+    print sym
+
+
+if __name__ == '__main__':
+
+    nd = 900
+    next_run = None
+    ticker = open('tickers.txt')
+    while 1:
+        if next_run != next_dt(datetime.datetime.now(pytz.utc), nd):
+            next_run = next_dt(datetime.datetime.now(pytz.utc), nd)
+            delay = (next_run - datetime.datetime.now(pytz.utc)).total_seconds()
+            threading.Timer(delay, lambda: send_request(ticker.readline().split()[0])).start()
+            print next_run, delay
+        
+        #sleep for 5 mins
+        time.sleep(nd/2)
--- process02.py
+++ process02.py
@@ -1,14 +1,34 @@
-import time
+from urllib2 import Request, urlopen
+import json
+from pymongo import MongoClient
 
 def go(client, pid):
 	size = 1024
 	rc = '000'
-	for x in range(100, 105): 
-	    print x
-	    time.sleep(5)
-	payload = client.recv(size)
-	print payload
+	stock_symbol = client.recv(size)
+	print stock_symbol
+        url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + stock_symbol + '.json?rows=1&api_key=tZFMcNCHtfsgXbv3tJX2'
+        print url
+        request = Request(url)
+        response = urlopen(request)
+
+        hit = response.read()
+
+        json_data = json.loads(hit)
+        record = dict(zip(json_data['dataset']['column_names'][:6], json_data['dataset']['data'][0][:6]))
+        record['symbol'] = json_data['dataset']['dataset_code']
+
+        mclient = MongoClient()
+
+        db_stock = mclient['stock']
+
+        col_daily = db_stock['daily']
+
+        result = col_daily.insert_one(record)
+        
 	# send reply and close connection
-	client.send('CC: {0}; {1}'.format(pid, rc))
+	client.send('CC: {0}; {1}; {2}'.format(rc, pid, result.inserted_id))
 	client.close()
-	return rc
+	return rc 
+
+
